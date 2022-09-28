c29f1c8475b3a2427047890f9bd173ca292b7569
kei_Seo <67668805+Kei-Seo@users.noreply.github.com>
2022-09-28 15:32:04 +0900

    inital commit

---
+++ src/test/java/com/looC/LooC/LooCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.looC.LooC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LooCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
